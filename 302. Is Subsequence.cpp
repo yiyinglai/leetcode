ecede8e5d201970897d8b4e6ea32f672fc781f14
Yiying Lai <yiying.lai@outlook.com>
2024-01-17 23:03:37 +0800

    Is Subsequence

---
+++ 302. Is Subsequence.cpp
@@ -0,0 +1,24 @@
+/*
+My solution.
+*/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) {
+                ++i;
+            }
+            ++j;
+        }
+        if (i != s.size()) {
+            return false;
+        }
+        return true;
+    }
+};
